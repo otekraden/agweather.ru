85e8974e0ff68e478a4dc894635dc0dd42876e39
Anton <skorodeev@gmail.com>
2022-07-16 02:28:35 +0300

    dump_db_to_clouds crt

--- datascraper/management/commands/dump_db_to_clouds.py
+++ datascraper/management/commands/dump_db_to_clouds.py
@@ -5,8 +5,7 @@ from dotenv import load_dotenv
 import os
 import tg_logger
 import zipfile
-import logging
-
+from datascraper.logging import init_logger
 
 from django.core import management
 
@@ -16,8 +15,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
+        logger = init_logger('Dump database')
+
+        logger.info("> START")
+
         dt = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
-        filename = "dump_db.json"
+        filename = f"{dt}_dump_db.json"
 
         with open(filename, "w") as f:
             management.call_command("dumpdata", stdout=f)
@@ -29,34 +32,18 @@ class Command(BaseCommand):
         load_dotenv()
         yandex = yadisk.YaDisk(token=os.environ["YANDEX_TOKEN"])
         yandex.upload(f'{filename}.zip',
-                      f'agweather_dump_db/{dt}_{filename}.zip')
-        
+                      f'agweather_dump_db/{filename}.zip')
+
         token = os.environ["TELEGRAM_TOKEN"]
         users = os.environ["TELEGRAM_USERS"].split('\n')
-
-        logger = logging.getLogger('foo')
-        logger.setLevel(logging.INFO)
-        # Logging bridge setup
-        tg_logger.setup(logger, token=token, users=users)
-        
         tg_files_logger = tg_logger.TgFileLogger(
             token=token,
             users=users,
             timeout=10
         )
+        tg_files_logger.send(f'{filename}.zip')
 
-        file_name = "test.txt"
-        with open(file_name, 'w') as example_file:
-            example_file.write("Hello from tg_logger by otter18")
+        os.remove(filename)
+        os.remove(f'{filename}.zip')
 
-        # Base logger
-
-
-        tg_files_logger.send(file_name, "Dump database")
-
-        # os.remove(filename)
-        # os.remove(f'{filename}.zip')
-
-
-        # print("{0} > Database dump of the Datascraper App created and saved".
-        #       format(datetime.now().isoformat(' ')))
+        logger.info("> END")

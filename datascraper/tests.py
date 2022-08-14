1a13003b0efa32897f44bf1f8b9e0d081344b6c5
Anton <skorodeev@gmail.com>
2022-08-14 22:45:18 +0300

    tests mod

--- datascraper/tests.py
+++ datascraper/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import ForecastTemplate, ArchiveTemplate
+from .forecasts import (
+    func_start_date_from_source,
+    month_rusname_to_number
+    )
+
+
+class ForecastTemplateModelTests(TestCase):
+    def test_func_start_date_from_source(self):
+        """
+        bla bla bla
+        """
+        start_datetime = func_start_date_from_source(
+            month=7,
+            day=5,
+            
+        )

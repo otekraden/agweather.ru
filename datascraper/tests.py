04119d6f06127334f26c7a78a04dcc30411a47ee
Anton <skorodeev@gmail.com>
2022-08-15 19:08:54 +0300

    requirements.txt recreated

--- datascraper/tests.py
+++ datascraper/tests.py
@@ -1,20 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
-from .models import ForecastTemplate, ArchiveTemplate
-from .forecasts import (
-    func_start_date_from_source,
-    month_rusname_to_number
-    )
+# from .models import ForecastTemplate, ArchiveTemplate
+# from .forecasts import (
+#     func_start_date_from_source,
+#     month_rusname_to_number
+#     )
 
 
-class ForecastTemplateModelTests(TestCase):
-    def test_func_start_date_from_source(self):
-        """
-        bla bla bla
-        """
-        start_datetime = func_start_date_from_source(
-            month=7,
-            day=5,
-            
-        )
+# class ForecastTemplateModelTests(TestCase):
+#     def test_func_start_date_from_source(self):
+#         """
+#         bla bla bla
+#         """
+#         start_datetime = func_start_date_from_source(
+#             month=7,
+#             day=5,
+#         )

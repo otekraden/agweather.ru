fa931d0c757ffc1125f764adfe5591d0ca969c34
Anton <skorodeev@gmail.com>
2022-11-06 18:48:19 +0300

    models mod: user email unique

--- website/models.py
+++ website/models.py
@@ -26,3 +26,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+User._meta.get_field('email')._unique = True
